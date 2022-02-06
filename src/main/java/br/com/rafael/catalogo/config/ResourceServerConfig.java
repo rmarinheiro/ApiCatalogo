de753d5b7f457aa0997ebdf29619d0382cdc1105
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-02-06 00:02:26 -0300

    Liberando o acesso ao h2

--- src/main/java/br/com/rafael/catalogo/config/ResourceServerConfig.java
+++ src/main/java/br/com/rafael/catalogo/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package br.com.rafael.catalogo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC = { "/oauth/token","/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/producties/**", "/categories/**" };
 	
@@ -29,6 +35,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//libera o h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
