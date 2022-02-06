75dc25760872ea21d580dedd7ce7d71d99b2ab5a
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-02-05 21:25:41 -0300

    Token additional information

--- src/main/java/br/com/rafael/catalogo/config/AuthoriztionServerConfig.java
+++ src/main/java/br/com/rafael/catalogo/config/AuthoriztionServerConfig.java
@@ -1,5 +1,7 @@
 package br.com.rafael.catalogo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import br.com.rafael.catalogo.components.JwtTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -38,6 +44,9 @@ public class AuthoriztionServerConfig extends AuthorizationServerConfigurerAdapt
 	@Autowired
 	private AuthenticationManager authenticatedManager;
 	
+	@Autowired
+	private JwtTokenEnhancer tokenEnhancer;
+	
 	
 
 	@Override
@@ -55,7 +64,11 @@ public class AuthoriztionServerConfig extends AuthorizationServerConfigurerAdapt
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticatedManager).tokenStore(tokenStore).accessTokenConverter(accessTokenConvertes) ;
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain(); 
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConvertes,tokenEnhancer));
+		
+		endpoints.authenticationManager(authenticatedManager).tokenStore(tokenStore).accessTokenConverter(accessTokenConvertes)
+		.tokenEnhancer(tokenEnhancerChain);
 		
 	}
 	
