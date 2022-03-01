1e04e1d7add40f419f41dd41f5bbb93a65513123
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-03-01 13:09:55 -0300

    Ajustes para o uso do Swagger

--- src/main/java/br/com/rafael/catalogo/config/ResourceServerConfig.java
+++ src/main/java/br/com/rafael/catalogo/config/ResourceServerConfig.java
@@ -22,7 +22,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private Environment env;
 	
-	private static final String[] PUBLIC = { "/oauth/token","/h2-console/**" };
+	private static final String[] PUBLIC = { "/oauth/token","/h2-console/**" ,"/swagger-ui.html/**" , "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+			"/swagger-ui.html", "/webjars/**"
+	};
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/producties/**", "/categories/**" };
 	
@@ -32,6 +34,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
 	}
+	
+	
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
